ee75b0383da7fabb7b437b31aa9802a0e3a92253
Zeerak Asim <Z_zee09@hotmail.com>
2022-11-27 14:20:49 -0500

    testing to see if commit worked -zee
    changed comment line for line 72 on app.js under the web_based_gui sub directory.

--- web_based_GUI/app.js
+++ web_based_GUI/app.js
@@ -69,7 +69,7 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // render the error page with its intended error status
   res.status(err.status || 500);
   res.render('error');
 });
